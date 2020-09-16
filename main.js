300f77a146dc279a18e8a5fbe7d326210786f0e3
Porcupine92 <szymonjozefowicz1992@gmail.com>
2020-09-16 22:07:23 +0200

    change code to be more objectiv

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const elements = document.querySelectorAll('.anim-bg');
 
-    document.addEventListener('mousemove', function (event) {
-        const { clientX, clientY } = event;
-        const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight / 2;
+    const balloon = new Balloon('.anim-bg');
 
-        elements.forEach(element => {
-            const ratioX = -element.getAttribute('ratioX');
-            const ratioY = -element.getAttribute('ratioY');
-
-            const moveX = clientX - centerX;
-            const moveY = clientY - centerY;
-
-            element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY
-                }px)`
-        })
-    })
+    document.addEventListener('mousemove', (event) => balloon.listenCursorMove(event));
 })
